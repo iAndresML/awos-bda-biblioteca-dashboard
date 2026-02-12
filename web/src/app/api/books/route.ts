0df5286c8e7750345de40a3d4e19668cccb42004
iAndresML <andres-mazariegos@hotmail.com>
2026-02-12 14:31:45 -0600

    implemento busqueda y paginacion en api books

--- web/src/app/api/books/route.ts
+++ web/src/app/api/books/route.ts
@@ -1,7 +1,35 @@
 import { pool } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const r = await pool.query("SELECT * FROM vw_most_borrowed_books");
-  return NextResponse.json(r.rows);
+export async function GET(req: Request) {
+
+  const { searchParams } = new URL(req.url);
+
+  const search = searchParams.get("search") || "";
+
+  const page = parseInt(searchParams.get("page") || "1");
+
+  const limit = parseInt(searchParams.get("limit") || "10");
+
+  const offset = (page - 1) * limit;
+
+  let query = `
+    SELECT *
+    FROM vw_most_borrowed_books
+    WHERE title ILIKE $1
+       OR author ILIKE $1
+    ORDER BY ranking
+    LIMIT $2 OFFSET $3
+  `;
+
+  const values = [
+    `%${search}%`,
+    limit,
+    offset
+  ];
+
+  const result = await pool.query(query, values);
+
+  return NextResponse.json(result.rows);
+
 }
